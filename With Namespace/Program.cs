dc16a25d0fd11ffcf0ed00041b9e4f751f6ccdb3
Arash zandi <zandiarash@hotmail.com>
2024-09-10 15:51:46 +0330

    more brief

--- With Namespace/Program.cs
+++ With Namespace/Program.cs
@@ -1,21 +1,9 @@
-using Asp.Versioning;
-using Asp.Versioning.Builder;
 using MinimalEndpoints.Extensions;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddApiVersioning(options =>
-{
-    options.DefaultApiVersion = new ApiVersion(1);
-    options.ApiVersionReader = new UrlSegmentApiVersionReader();
-}).AddApiExplorer(options =>
-{
-    options.GroupNameFormat = "'v'V";
-    options.SubstituteApiVersionInUrl = true;
-});
 
 WebApplication app = builder.Build();
 
